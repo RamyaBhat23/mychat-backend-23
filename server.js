92fdb6089cdfcba98d5f50792b1f543823088ff3
RamyaBhat23 <ramya.shi.1111@gmail.com>
2020-10-16 21:04:21 +0530

    mychat backend

--- server.js
+++ server.js
@@ -93,6 +93,6 @@ io.on('connection', (socket) => {
     });
 });
 
-http.listen(port, () => {
-    console.log('listening on *:3000');
-});
+// http.listen(port, () => {
+//     console.log('listening on *:3000');
+// });
