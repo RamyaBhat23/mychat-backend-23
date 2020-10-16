333498aeb7412431d384832785312f923c27abe3
RamyaBhat23 <ramya.shi.1111@gmail.com>
2020-10-16 21:30:51 +0530

    mychat backend

--- server.js
+++ server.js
@@ -17,11 +17,6 @@ app.get('/', (req, res) => {
     res.end();
 });
 
-// app.listen(process.env.PORT || 8080, () => {
-//     console.log("http://localhost:8080");
-//     //https://localhost:8080 doesn't work
-// });
-
 function getRooms() {
     roomRead = fs.readFileSync('rooms.json', 'utf-8');
     roomData = JSON.parse(roomRead);
